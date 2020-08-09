c975a5621a3bdb6d172f24634376e148b14271e3
Arpandeep Khatua <akhatua2@illinois.edu>
2020-08-09 23:58:12 +0400

    some changes

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,5 @@ import "package:flutter/material.dart";
 
 void main() {
   configureLogger();
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: CirclesApp(),
-    );
-  }
+  runApp(CirclesApp());
 }
